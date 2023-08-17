1edfc37c57841650740cea21a9826f009a1da629
jj-vinaysaran <vinayjayjay12@gmail.com>
2023-08-17 10:19:23 +0530

    sponsor check

--- src/Pages/Sponsors/Sponsors.jsx
+++ src/Pages/Sponsors/Sponsors.jsx
@@ -11,89 +11,32 @@ import { FaGithub } from "react-icons/fa";
                 <>
                 <section className="card">
                     <div className="card-container">
-                        <div className="card-box">
-                            <img src={cards} alt=""/>
-                            <div className="card-layer">
-                                <h4>Sponsor name</h4>
-                                <p>Sponsor details</p>
-                                    <a href="https://github.com/Lordoflegend2003/Online-E-Commerce-tracking-System-OETS-"><BsLink45Deg className="i"/></a>
-                            </div>
-                    </div>
                     <div className="card-box">
                         <img src={cards} alt="" />
                         <div className="card-layer">
-                        <h4>Sponsor name</h4>
-                            <p>Sponsor details</p>
+                        <h4 className="sponsh4">Sponsor name</h4>
+                            <p className="sponsp">Sponsor details</p>
                             <a href="https://github.com/Lordoflegend2003/Online-E-Commerce-tracking-System-OETS-"><BsLink45Deg className="i"/></a>
                         </div>
                     </div>
-                    <div className="card-box">
-                        <img src={cards} alt="" />
-
-                        <div className="card-layer">
-                        <h4>Sponsor name</h4>
-                            <p>Sponsor details</p>
-                            <a href="https://github.com/Lordoflegend2003/Online-E-Commerce-tracking-System-OETS-"><BsLink45Deg className="i"/></a>
-                        
-                        </div>
-                    </div>
-                    <div className="card-box">
+                       <div className="card-box">
                         <img src={cards} alt="" />
                         <div className="card-layer">
-                        <h4>Sponsor name</h4>
-                            <p>Sponsor details</p>
-                            <a href="https://github.com/Lordoflegend2003/Online-E-Commerce-tracking-System-OETS-"><BsLink45Deg className="i" /></a>
-                            
+                        <h4 className="sponsh4">Sponsor name</h4>
+                            <p className="sponsp">Sponsor details</p>
+                            <a href="https://github.com/Lordoflegend2003/Online-E-Commerce-tracking-System-OETS-"><BsLink45Deg className="i"/></a>
                         </div>
                     </div>
-                    <div className="card-box">
+                       <div className="card-box">
                         <img src={cards} alt="" />
                         <div className="card-layer">
-                        <h4>Sponsor name</h4>
-                            <p>Sponsor details</p>
-                            <a href="https://github.com/Lordoflegend2003/Online-E-Commerce-tracking-System-OETS-"><BsLink45Deg className="i"/></a>
-                            
-                        </div>
-                    </div>
-                    <div className="card-box">
-                        <img src={cards}  alt=""/>
-
-                        <div className="card-layer">
-                        <h4>Sponsor name</h4>
-                            <p>Sponsor details</p>
-                            <a href="https://github.com/Lordoflegend2003/Online-E-Commerce-tracking-System-OETS-"><BsLink45Deg className="i"/></a>
-                        </div>
-                    </div>
-                    <div className="card-box">
-                        <img src={cards}  alt=""/>
-
-                        <div className="card-layer">
-                        <h4>Sponsor name</h4>
-                            <p>Sponsor details</p>
-                            <a href="https://github.com/Lordoflegend2003/Online-E-Commerce-tracking-System-OETS-"><BsLink45Deg className="i"/></a>
-                        
-                        </div>
-                    </div>
-                    <div className="card-box">
-                        <img src={cards}  alt=""/>
-
-                        <div className="card-layer">
-                        <h4>Sponsor name</h4>
-                            <p>Sponsor details</p>
+                        <h4 className="sponsh4">Sponsor name</h4>
+                            <p className="sponsp">Sponsor details</p>
                             <a href="https://github.com/Lordoflegend2003/Online-E-Commerce-tracking-System-OETS-"><BsLink45Deg className="i"/></a>
                         </div>
                     </div>
                 </div>
             </section>
-             <div className="developers">
-                <h1 className="dev_title">Developers</h1> 
-                <div className="dev_card_parent">
-                    <div className="dev_card"><h1 className="dev_name">Arunkumar</h1><h3 className="dev_role">Full Stack developer</h3><div className="dev_icons"><a  href=""><FaInstagram/></a> <a href="#"><FaLinkedinIn/></a> <a href="#"><FaGithub/></a></div></div>
-                    <div className="dev_card"><h1 className="dev_name">B Pawan Kalyan</h1><h3 className="dev_role">Full Stack developer</h3><div className="dev_icons"><a  href=""><FaInstagram/></a> <a href="#"><FaLinkedinIn/></a> <a href="#"><FaGithub/></a></div></div>
-                    <div className="dev_card"><h1 className="dev_name">Vinay Saran JJ</h1><h3 className="dev_role">Full Stack developer</h3><div className="dev_icons"><a  href=""><FaInstagram/></a> <a href="#"><FaLinkedinIn/></a> <a href="#"><FaGithub/></a></div></div>
-                    <div className="dev_card"><h1 className="dev_name">S Sameerul Hak</h1><h3 className="dev_role">Full Stack developer</h3><div className="dev_icons"><a  href=""><FaInstagram/></a> <a href="#"><FaLinkedinIn/></a> <a href="#"><FaGithub/></a></div></div>
-                </div>
-             </div>
             </>
 
         );
