b8e2b5d075d8abcdd1cdbf2efa2ef67084793a3e
lordoflegend2003 <bpawankalyan.it2021@citchennai.net>
2023-08-29 09:53:43 +0530

    create

--- src/Pages/HomePage/HomeIntro.jsx/HomeIntro.jsx
+++ src/Pages/HomePage/HomeIntro.jsx/HomeIntro.jsx
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom'
 function HomeIntro() {
   return (
     <div className="herohomecontainer">
-      <p className="heroPTag"><span className='home_tags'>{"< "}</span>DEPARTMENT OF<span className='home_tags'>{" />"}</span></p>
+      {/* <p className="heroPTag"><span className='home_tags'>{"< "}</span>DEPARTMENT OF<span className='home_tags'>{" />"}</span></p>
       <h3 id="dept-intro"><span className='home_tags'>{"< "}</span>INFORMATION TECHNOLOGY<span className='home_tags'>{" />"}</span></h3>
       <p className="heroPTag"> <span className='home_tags'>{"< "}</span>PRESENTS<span className='home_tags'>{" />"}</span></p>
       <img src={Trojansimg} id="logo_trojans"  alt=""/>
@@ -19,7 +19,36 @@ function HomeIntro() {
       <div className="scroll">
         <h2 className="scroll_intro">SCROLL DOWN</h2>
         <img src={scrol} alt="" className="scroll_img"></img>
-      </div>
+      </div> */}
+
+      <div className="logo-wrap">
+  <svg
+    version="1.1"
+    id="Слой_1"
+    xmlns="http://www.w3.org/2000/svg"dcfvvbn n nḍśṣṅḥl̥ṁr̥ṭṇ̃ē.nm,;b vl'gfhdcxnfvb 
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 855 150"
+    style={{ enableBackground: "new 0 0 855 150" }}
+    xmlSpace="preserve"
+  >
+    <style>
+      {`
+        .st0{fill:none;stroke:#FFFFFF;stroke-width:2;stroke-miterlimit:10;}
+        .st1{font-family:'open sans';}
+        .st2{font-size:150px;}
+      `}
+    </style>
+    <text
+      transform="matrix(1 0 0 1 0 125.5508)"
+      className="st0 st1 st2"
+    >
+      TROJANS
+    </text>
+  </svg>
+</div>;
+
     </div>
   );
 }
